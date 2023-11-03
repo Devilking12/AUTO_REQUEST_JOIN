c79a5f423bdc703f92cf3bd9694659ad17b2163f
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-03 23:50:05 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,37 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, User, ChatJoinRequest
+
+app=Client(
+    "𝗕𝗼𝘁 𝗦𝘁𝗮𝗿𝘁𝗲𝗱",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+CHAT_ID=int(os.environ.get("CHAT_ID", None))
+TEXT=os.environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved")
+APPROVED = os.environ.get("APPROVED_WELCOME", "on").lower()
+
+@app.on_message(filters.private & filters.command(["start"]))
+async def start(client: pr0fess0r_99, message: Message):
+    approvedbot = await client.get_me() 
+    button=[[
+      InlineKeyboardButton("𝚄𝙿𝙳𝙰𝚃𝙴𝚉", url="https://t.me/"),
+      InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃", url="https://t.me/")
+      ],[
+      InlineKeyboardButton("𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴", url=f"")
+      ]]
+    await message.reply_text(text="𝙷𝙴𝙻𝙻𝙾...⚡\n\n𝙸𝙰𝙼 𝙰 𝚂𝙸𝙼𝙿𝙻𝙴 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼 𝙰𝚄𝚃𝙾 𝚁𝙴𝚀𝚄𝙴𝚂𝚃 𝙰𝙲𝙲𝙴𝙿𝚃 𝙱𝙾𝚃.\n𝙵𝙾𝚁 𝚈𝙾𝚄𝚁 𝙲𝙷𝙰𝚃𝚂 𝙲𝚁𝙴𝙰𝚃𝙴 𝙾𝙽𝙴 𝙱𝙾𝚃", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
+
+@app.on_chat_join_request(filters.chat(CHAT_ID))
+async def autoapprove(client: app, message: ChatJoinRequest):
+    chat=message.chat # Chat
+    user=message.from_user # User
+    print(f"{user.first_name} 𝙹𝙾𝙸𝙽𝙴𝙳 ⚡") # Logs
+    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
+    if APPROVED == "on":
+        await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))       
+
+print("𝗕𝗼𝘁 𝗦𝘁𝗮𝗿𝘁𝗲𝗱 ")
+app.run()
